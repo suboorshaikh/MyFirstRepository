5979a1ddb957669940c4c3c4d7e207ccd0c45678
Suboor shaikh <Suboor shaikh@LAPTOP-U202SUFC>
2023-10-31 22:35:26 +0530

    TYPE CONVERSION

--- HOMEWORK/CH2 DATEATYPE.cs
+++ HOMEWORK/CH2 DATEATYPE.cs
@@ -15,12 +15,6 @@ public class CH2_DATEATYPE
     {
         IntegerDataType();
     }
-
-    private static void IntegerDataType()
-    {
-        throw new NotImplementedException();
-    }
-
     public static void NumberSystem()
     {
         ///There are basically four number system
@@ -59,54 +53,63 @@ public class CH2_DATEATYPE
         ///Signed long              : 2^64 = 18446744073709551616/2 = (-9223372036854775808 to 9223372036854775807)
         ///Unsigned Int128 2^128 = 340282366920938463463374607431768211456
         ///Signed Int128 2^128/2 = (-170141183460469231731687303715884105728 to 170141183460469231731687303715884105727)
-        public static void IntegerDataType()
-        {
-            Byte ub = 255;
-            SByte b = -128; b = 127;
-            UInt16 ui = 65535;
-            Int16 i = -32768; i = 32767;
 
-            ///HOMEWORK DONE
-            UInt32 ua = 4294967296;
-            Int32 a = -2147483648; a = 2147483647;
-            UInt64 ub = 18446744073709551616;
-            Int64 c = -9223372036854775808; c = 9223372036854775807;
-            UInt128 ud = 340282366920938463463374607431768211456;
-            Int128 e = -170141183460469231731687303715884105728; e = 170141183460469231731687303715884105727
-            //Homework: write variables with max capacity for following types: int32, int 64, int128
 
-        }
-        public static void FloatingDataType()
-        {
-            //We have three data types in this category float, double and decimal
-            //1. float 4bytes
-            float f = 1.1f;//in float a number 1.1 to be appended by f
-                           //2. double 8bytes
-            double d = 1.1d;//in double a number 1.1 may be appended by d
-            double d1 = 1.1;
-            //3. decimal 16bytes
-            decimal m = 1.1m;//in decimal a number 1.1 to be appended by m
-        }
-        public static void CharacterDataType()
-        {
-            //This data type is used to store a character from the list of ASCII characters
-            char c = '1';//here 1 as a char will be saved as decimal 49. Refer chart
-        }
-        public static void BooleanDataType()
-        {
-            //
-            bool b = true;//It will be saved as 1
-            bool b1 = false;//It will be saved as 0
-        }
+    }
+    public static void IntegerDataType()
+    {
+        Byte ub = 255;
+        ub = 1;
+        byte ubbyte = 255;
+        SByte b = -128; b = 127;
 
-        public static void ReferencedDataType()
-        {
-            //String data type : Array of Character 
-            // if i want to save a sentence "hello from .net class"
-            string s = "hello from .net class";
-            string greet = "Hello";
-            //there are some more referenced data types other than string
-            //for example Class object, interfaces, etc
-        }
+        UInt16 ui16 = 65535;//short
+        ushort ui16short = 65535;
+        Int16 i16 = -32768; i16 = 32767;
+        //Homework: write variables with max capacity for following types: int32, int 64, int128
+        UInt32 ui32 = 4294967295;
+        uint ui32int = 4294967295;
+        Int32 si32 = -2147483648; si32 = 2147483647;
+        UInt64 ui64 = 18446744073709551615;
+        ulong ui64long = 18446744073709551615;
+        Int64 si64 = -9223372036854775808; si64 = 9223372036854775807;
+        //UInt128 ui128 = 340282366920938463463374607431768211455;
+        //Int128 si128 = -170141183460469231731687303715884105728; b = 170141183460469231731687303715884105727;
+
+    }
+    public static void FloatingDataType()
+    {
+        //We have three data types in this category float(Single), double(Double) and decimal(Decimal)
+        //1. float 4bytes
+        float f = 1.1f;//in float a number 1.1 to be appended by f
+        //2. double 8bytes
+        double d = 1.1d;//in double a number 1.1 may be appended by d
+        double d1 = 1.1;
+        //3. decimal 16bytes
+        decimal m = 1.1m;//in decimal a number 1.1 to be appended by m
+    }
+    public static void CharacterDataType()
+    {
+        //This data type is used to store a character from the list of ASCII characters
+        char c = '1';//here 1 as a char will be saved as decimal 49. Refer chart
+    }
+    public static void BooleanDataType()
+    {
+        //
+        Boolean b = true;//It will be saved as 1
+        bool b1 = false;//It will be saved as 0
+    }
+
+    public static void ReferencedDataType()
+    {
+        //String data type : Array of Character 
+        // if i want to save a sentence "hello from .net class"
+        string s = "hello from .net class";
+        string greet = "Hello";
+        //there are some more referenced data types other than string
+        //for example Class object, interfaces, etc
+        CH02_DATATYPE ch02;//ch02 is a reference variable holding null value
+        //We will learn the how to add data to class which will be stored in heap
+        //in OOPS
     }
 }
---
+++ HOMEWORK/CH3 TYPE CONVERSION.cs
@@ -0,0 +1,129 @@
+﻿namespace HOMEWORK;
+/// <summary>
+/// Type conversion is of two types (Valued Datatypes)
+/// 1. Implicit - Automatic (the conversion that does not result in any loss are converted
+/// automatically.)
+/// 2. Explicit - Manual (the conversion that has a chance of any loss are not converted 
+/// automatically and needs to be converted manually)
+/// 3. Using Convert Class
+/// 4. Using Parse Method
+/// 5. Using TryParse Method (BEST) - Incomplete
+/// </summary>
+public class CH3_TYPE_CONVERSION
+{
+    public static void Test()
+    {
+        ConversionUsingConvertClass();
+    }
+    public static void ImplicitConversion()
+    {
+        byte b = 255;       //8bit
+        short s = b;        //16bit
+        int i = s;          //32bit
+        long l = i;         //64bit
+        Int128 int128 = l;  //128bit
+
+        float f = i;
+        double d = i;
+        decimal m = i;
+
+        char c = 'a';       //'a' is 97 refer ASCII Chart
+        i = c;              //int32
+        l = c;              //long64
+        int128 = c;         //Int128
+    }
+    public static void ExplicitConversion()
+    {
+        //This process requires casting/parsing
+        //BE CAREFUL WHILE EXPLICIT CONVERSION AS RESULTS MAY 
+        //BE UNPREDICTABLE
+        Int128 int128 = 1;
+        long l = (long)int128; //this will cast int128 into long
+        int i = (int)l;
+        short s = (short)i;
+        byte b = (byte)s;
+
+        ///following conversion is unpredictable as 
+        ///8 bits from short(16bits) are copied into byte(8bit)
+        s = 200;
+        sbyte b1 = (sbyte)s;
+        Console.WriteLine($"short {s} is converted into byte {b1}");
+
+        float f = 4.9999f;
+        i = (int)f;
+        Console.WriteLine($"float {f} is converted into int {i}");
+
+        i = 97;//be careful while conversion as number beyond capacity will give unpredictable results
+        char c = (char)i;
+        Console.WriteLine($"int {i} is converted into char {c}");
+        //byte byte1 = 1;
+        //bool b2 = (bool)byte1;
+        //for boolean conversion we have to use Convert class
+
+    }
+    public static void ConversionUsingConvertClass()
+    {
+        string str = "";
+
+        //To Integer
+        //1) to Byte
+        str = "1";
+        byte by = Convert.ToByte(str);
+        //2) to short
+        short s = Convert.ToInt16(str);
+        //3) to int
+        int i = Convert.ToInt32(str);
+        //4) to long
+        long l = Convert.ToInt64(str);
+        //5) to Int128
+        //Int128 int128 = Convert.ToInt128("str");//not yet ready from Microsoft
+
+        //To Floating/fractional numbers
+        str = "1.2";
+        float f = Convert.ToSingle(str);//float
+        double d = Convert.ToDouble(str);
+        decimal m = Convert.ToDecimal(str);
+
+        //To Character
+        str = "a";
+        char c = Convert.ToChar(str);
+
+        //To Boolean
+        //from string to bool
+        str = "true";
+        bool b = Convert.ToBoolean(str);
+        //from int to bool
+        i = 10;
+        b = Convert.ToBoolean(i);
+    }
+    public static void ConvertUsingParse()
+    {
+        ///Parse Method will convert the valid input otherwise it will
+        ///throw an exception (error)
+        string str = "";
+        //To Integer
+        str = "12";
+        byte b = byte.Parse(str);
+        short s = short.Parse(str);
+        int i = int.Parse(str);
+        long l = long.Parse(str);
+        Int128 int128 = Int128.Parse(str);
+        //To Floating
+        float f = float.Parse(str);
+        Double d = double.Parse(str);
+        Decimal m = Decimal.Parse(str);
+        //To Char
+        str = "1";
+        char c = char.Parse(str);
+        //To boolean
+        str = "TRUE";
+        bool bl = bool.Parse(str);
+    }
+    //Next is tryparse
+    public static void ConvertUsingTryParse()
+    {
+        ///Before studying TryParse you must know If-Else and 
+        ///Call by Reference using out
+    }
+
+}
