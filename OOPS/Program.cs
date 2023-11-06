35d01406de9ddacc37be2c6ceb3fcbfbd0fee3c5
Suboor shaikh <Suboor shaikh@LAPTOP-U202SUFC>
2023-11-06 22:37:30 +0530

    update

--- OOPS/Program.cs
+++ OOPS/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using OOPS_Project;
+CH05_Methods cH05_Methods = new CH05_Methods();
+cH05_Methods.Test();
